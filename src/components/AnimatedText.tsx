8c86178a92ccb2d50be75b7d762c77c23ea41a5c
Eric Geng <egeng@umich.edu>
2026-01-20 18:20:04 -0500

    checked for more ease occurances

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -43,7 +43,7 @@ export function AnimatedText({
               y: 0,
               transition: {
                 duration: 0.4,
-                ease: [0.22, 1, 0.36, 1],
+                ease: "easeInOut",
               },
             },
           }}
@@ -92,7 +92,7 @@ export function AnimatedWords({
               rotateX: 0,
               transition: {
                 duration: 0.5,
-                ease: [0.22, 1, 0.36, 1],
+                ease: "easeInOut",
               },
             },
           }}
